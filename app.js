1e3d5fda42cfe50a54225084087338c62c9d58f9
MuhammadHuzaifa22 <153933649+MuhammadHuzaifa22@users.noreply.github.com>
2024-02-29 09:55:55 +0500

    If Else
    IF Else

---
+++ app.js
@@ -0,0 +1,14 @@
+var amount = +prompt("Enter Your Amount")
+if  (amount > 10000000){
+console.log("Mubarak Ko Ho Apko Ghar Mil jay Ga");
+}
+else if (amount >= 9000000){
+    console.log("bhai kuch or barha kar de do phr ghar ki cahbi le lo");
+}
+else if(amount >= 5000000){
+    console.log("sir mene dosri party ko bech diya");
+}
+else
+{
+    console.log("sorry");
+}
