95b79ea2ade361cd63e6697bdd26b5680815f285
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-11-01 00:21:19 +0500

    work added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-var amount = +prompt("Enter Your Amount")
-if  (amount > 10000000){
-console.log("Mubarak Ko Ho Apko Ghar Mil jay Ga");
+
+const div = document.getElementById('result-div')
+const form = document.getElementById('form');
+const inputVal = document.getElementById('inputVal')
+
+// Form Submit Function
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (inputVal == null) return;
+if  (inputVal.value >= 10000000){
+div.innerHTML=`Result: <span class="text-green-600">Congratulations to you, you will get a house</span>`;
 }
-else if (amount >= 9000000){
-    console.log("bhai kuch or barha kar de do phr ghar ki cahbi le lo");
+else if (inputVal.value >= 9000000){
+    div.innerHTML = `Result: <span class="text-green-600">Congrats! May you soon get your dream home—key in hand!</span>`;
 }
-else if(amount >= 5000000){
-    console.log("sir mene dosri party ko bech diya");
+else if(inputVal.value >= 5000000){
+    div.innerHTML=`Result: <span class="text-red-500">I can't give you this.</span>`
 }
 else
 {
-    console.log("sorry");
+    div.innerHTML=`Result: <span class="text-red-500">sorry</span>`;
 }
+
+})
+
+
+// var amount = +prompt("Enter Your Amount")
